3396df129cf5ebb071374e634525ad43626fe02c
Binecsbk20 <bientran@MacBook-Pro-cua-Bien.local>
2022-04-18 22:15:29 +0700

    upload

---
+++ Manage_Library.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+
+class Book{
+    int bookId;
+    String bookName;
+    String writerName;
+    int price;
+    int quantity;
+    bool borrowed;
+    public Book(int id, String bN, String wN, int pr, int quan){
+        bookId = id;
+        bookName = bN;
+        writerName = wN;
+        price = pr;
+        quantity = quan;
+    }
+}
+class Admin{
+    String Name;
+    String Passwork;
+    Hashtable<Interger, String> User_Book_Lent; // Lưu id, và tên của user mượn sách. 
+    
+}
+class User{
+        String Name;
+        String Passwork;
+        Vector<Int> Book_Borrowed; // Tên sách mà user mượn. Tối đa 5 quyển sách. 
+}
+class Library{
+    Vector<Book> books;
+    Vector<User> users;
+    Vector<Admin> admin;
+
+}
+
+public class Manage_Library {
+    int n = 5;
+    public static void main(String[] args){
+        
+    }
+}
