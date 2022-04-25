a613d5ceb3b11e048dcd4913db30428a4c638f02
Bine Tran <77770500+Binecsbk20@users.noreply.github.com>
2022-04-25 13:01:42 +0700

    Update Manage_Library.java

--- Manage_Library.java
+++ Manage_Library.java
@@ -6,10 +6,8 @@ class Book{
     int bookId;
     String bookName;
     String writerName;
-    int price;
-    int quantity;
     boolean borrowed;
-    public Book(int id, String bN, String wN, int pr, int quan){
+    public Book(int id, String bN, String wN, int pr){
         bookId = id;
         bookName = bN;
         writerName = wN;
@@ -20,19 +18,23 @@ class Book{
 class Admin{
     String Name;
     String Passwork;
-    Hashtable<Interger, String> User_Book_Lent; // Lưu id, và tên của user mượn sách. 
-    
-}
-class User{
-        String Name;
-        String Passwork;
-        Vector<Integer> Book_Borrowed; // Tên sách mà user mượn. Tối đa 5 quyển sách. 
+    Hashtable<Integer, String> User_Book_Lent; // Lưu id, và tên của user mượn sách. 
+    public Admin(String name, String pass){
+        User_Book_Lent = new Hashtable<Integer, String>();
+        Name = name;
+        Passwork = pass;
+    }
 }
 class Library{
     Vector<Book> books;
-    Vector<User> users;
     Vector<Admin> admin;
-
+    public Library(){
+        books = new Vector<Book>();
+        admin = new Vector<Admin>();
+    }
+    public void add_Book(Book bookNeedAdd){
+        Win.books.add(bookNeedAdd);
+    }
 }
 
 public class Manage_Library {
