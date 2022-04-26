4eb7060ca2d188acc133ad3e479e1782cf76fe50
Binecsbk20 <bientran@MacBook-Pro-cua-Bien.local>
2022-04-26 07:22:40 +0700

    upload

--- BTL/Manage_Library.java
+++ BTL/Manage_Library.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.util.*;
 
 
-class Book{
-    int bookId;
-    String bookName;
-    String writerName;
-    boolean borrowed;
+public class Book{
+    private int bookId;
+    private String bookName;
+    private String writerName;
+    private boolean borrowed;
     public Book(int id, String bN, String wN, int pr){
         bookId = id;
         bookName = bN;
@@ -15,24 +15,24 @@ class Book{
         quantity = quan;
     }
 }
-class Admin{
-    String Name;
-    String Passwork;
-    Hashtable<Integer, String> User_Book_Lent; // Lưu id, và tên của user mượn sách. 
+public class Admin{
+    private String Name;
+    private String Passwork;
+    private Hashtable<Integer, String> User_Book_Lent; // Lưu id, và tên của user mượn sách. 
     public Admin(String name, String pass){
         User_Book_Lent = new Hashtable<Integer, String>();
         Name = name;
         Passwork = pass;
     }
 }
-class Library{
-    Vector<Book> books;
-    Vector<Admin> admin;
+public class Library{
+    private Vector<Book> books;
+    private Vector<Admin> admin;
     public Library(){
         books = new Vector<Book>();
         admin = new Vector<Admin>();
     }
-    public static void add_Book(Book bookNeedAdd){
+    public void add_Book(Book bookNeedAdd){
         Win.books.add(bookNeedAdd);
     }
 }
